999e830eaa523b0aa451710c3ede010f0fe9cabf
haikalwahyudi <cocakun459@gmail.com>
2020-11-18 10:38:50 +0800

    menambahkan package dan memberi izin notifikasi

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -12,3 +12,40 @@ if ("serviceWorker" in navigator) {
 } else {
     console.log(`ServiceWorker belum didukung browser ini.`);
 }
+
+if ("Notification" in window) {
+    requestPermission();
+} else {
+    console.error("Browser tidak mendukung notifikasi.");
+}
+
+// Meminta ijin menggunakan Notification API
+function requestPermission() {
+    Notification.requestPermission().then(function (result) {
+        if (result === "denied") {
+            console.log("Fitur notifikasi tidak diijinkan.");
+            return;
+        } else if (result === "default") {
+            console.error("Pengguna menutup kotak dialog permintaan ijin.");
+            return;
+        }
+
+        console.log("Fitur notifikasi diijinkan.");
+    });
+    if (('PushManager' in window)) {
+    navigator.serviceWorker.getRegistration().then(function(registration) {
+        registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: << App Server Key >>
+        }).then(function(subscribe) {
+            console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
+            console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
+                null, new Uint8Array(subscribe.getKey('p256dh')))));
+            console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
+                null, new Uint8Array(subscribe.getKey('auth')))));
+        }).catch(function(e) {
+            console.error('Tidak dapat melakukan subscribe ', e.message);
+        });
+    });
+}
+}
--- service-worker.js
+++ service-worker.js
@@ -73,3 +73,24 @@ self.addEventListener("activate", event => {
         })
     );
 });
+
+self.addEventListener('push', function (event) {
+    var body;
+    if (event.data) {
+        body = event.data.text();
+    } else {
+        body = 'Pesan Push Tanpa Payload';
+    }
+    var options = {
+        body: body,
+        icon: 'assets/image/iconpwa.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        }
+    };
+    event.waitUntil(
+        self.registration.showNotification('Push Notification', options)
+    );
+});
